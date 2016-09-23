bad11cbe06f17265e31a4c370fb5705e4b2532ff
Mitch Woodard <woodardcommamitch@gmail.com>
2016-09-23 11:23:10 -0500

    Mostly complete update

--- sketch.js
+++ sketch.js
@@ -1,28 +1,61 @@
 $(document).ready(function(){
-	userSquares= prompt("How many squares?")
+	userSquares= prompt("How many squares(squared)?")
 	
+	var rainbowCount=0;
 	createSquares(userSquares);
 	
-	$('.squares').mouseover(function() {
-        $(this).addClass('squaresOn');
-		/*$(this).css("background-color", getRandomColor())*/
+	 $("#thetable").on('mouseenter', '.squares', function() {
+		 /*$(this).addClass('squaresOn');*/
+		 $(this).css("background-color", getRandomColor());
+        
     });
+	
 	
 	$('#switch').click(function(){
 		$('.squares').toggleClass('squaresOn');	
 	});
 	$('#reset').click(function(){
-		$('.squares').removeClass('squaresOn');	
+		$('.squares').css("background-color", "white");
 		
 	});
 });
 
 function createSquares(numSquares){
 	
-	for(i=0;i< numSquares*numSquares;i++){
-		$('#container').append("<div class=" + "squares"+ "></div>");	
-	}
-	$('#container').width(numSquares * 28 + "px");
+	/*Initialize and set size variables*/
+	var containerHeight=500+(numSquares*2);
+	var containerWidth=500 +(numSquares*2);
+
+	/*Box Sizes*/
+	var boxWidth=Math.floor(containerWidth/numSquares);
+	var boxHeight=containerHeight/numSquares -2;
+	
+	/*Creates Container Dimensions*/
+	console.log(containerHeight);
+	console.log(containerWidth);
+	console.log(boxHeight);
+	console.log(boxWidth);
+	console.log("Supposed height: " + boxWidth*numSquares)
+	
+	$('#container').width(containerWidth + 'px');
+	$('#container').height(containerHeight + 'px');
+	
+	$(document).ready(function(){
+		for (i = 1; i <= numSquares; i++) {
+			$("#thetable").append("<tr class=\"rows\"></tr>");
+		}
+		
+		for (y = 1; y <= numSquares; y++) {
+			$(".rows").append("<td class=\"squares\"></td>");
+		}
+		
+		$('.rows .squares').width(boxWidth + 'px');	
+		$('.squares').height(boxHeight +'px');
+	
+});
+	
+	
+	
 }
 
 function getRandomColor() {
@@ -32,4 +65,13 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function getRainbow(){
+	var rainbow = ["red","orange","yellow", "green","blue","violet"];
+	var color='';
+	
+	color += rainbow[Math.floor(Math.random() * 6)];
+	return color;
+	
 }
